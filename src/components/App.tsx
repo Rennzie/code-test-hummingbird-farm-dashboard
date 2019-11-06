2747f3a3163f17f4f0576318c27d8bf362de8e33
Sean.Rennie <sean.rennie6@gmail.com>
2019-11-06 20:38:22 +0200

    add: reavix charts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Map as LeafletMap, TileLayer } from 'react-leaflet';
+import { BarChart } from 'reaviz';
 import 'leaflet/dist/leaflet.css';
 
 import { useFarmState } from '../store';
@@ -31,18 +32,30 @@ function App() {
     farmState: { farmYield, fields }
   } = useFarmState();
 
+  const [chartData, setChartData] = useState([{ key: '', data: 0 }]);
+
+  useEffect(() => {
+    const newChartData = fields.map(field => ({
+      key: field.name,
+      data: field.yield ? field.yield : 0
+    }));
+
+    setChartData(newChartData);
+  }, [fields, setChartData]);
+
   return (
     <main className={classes.appWrapper}>
       <section className={classes.dashPanel}>
         <Typography variant="h4">{farm.name}</Typography>
         <Typography variant="h6">Estimated Yield: £{farmYield}</Typography>
         <SelectedFieldView />
+        <BarChart height={300} width={300} data={chartData} />
       </section>
       <LeafletMap
         style={{ width: '70vw', height: '100vh' }}
         // @ts-ignore
         center={farm.centre.coordinates}
-        zoom={13}
+        zoom={14}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {fields.map(field => (
