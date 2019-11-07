54f4a9f8e793b0a269fdca407f68da2f992556a3
Sean.Rennie <sean.rennie6@gmail.com>
2019-11-07 13:09:30 +0200

    Updates

---
+++ precache-manifest.e752b286ee299d3f1b0088dc23b819d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df588707f06f577bd5e5a8a25ac2854",
+    "url": "/code-test-hummingbird-farm-dashboard/./index.html"
+  },
+  {
+    "revision": "9b7556c6bdbf198f1c31",
+    "url": "/code-test-hummingbird-farm-dashboard/./static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "9b7556c6bdbf198f1c31",
+    "url": "/code-test-hummingbird-farm-dashboard/./static/js/2.5d27976e.chunk.js"
+  },
+  {
+    "revision": "ae55b1941a209cded850",
+    "url": "/code-test-hummingbird-farm-dashboard/./static/js/main.4751e591.chunk.js"
+  },
+  {
+    "revision": "52744db5e0c85b9e6bc8",
+    "url": "/code-test-hummingbird-farm-dashboard/./static/js/runtime-main.213d5004.js"
+  }
+]);
